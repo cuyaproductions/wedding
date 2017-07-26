1788000c8549c63a003ebba59998b410361fd4c8
Diego Hernandez <Diego.Hernandez@ibm.com>
2017-07-25 23:49:58 -0500

    [WIP] make current site responsive

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,4 +1,30 @@
 function nav () {
+  const navList = document.querySelector('.nav__list');
+  const navOverlay = document.querySelector('.nav__overlay');
+  const navTrigger = document.querySelector('.nav__trigger');
+
+  navTrigger.addEventListener('click', () => {
+    navOverlay.classList.toggle('nav__overlay--visible');
+    navList.classList.toggle('nav__list--visible');
+    navTrigger.classList.toggle('nav__trigger--active');
+
+    const triggerIsActive = navTrigger.classList.contains('nav__trigger--active');
+    navTrigger.textContent = triggerIsActive ? navTrigger.getAttribute('data-close') 
+      : navTrigger.getAttribute('data-open');
+    
+    navTrigger.style.transform = triggerIsActive ? 
+      `translateX(-${navList.offsetWidth}px) rotate(-90deg)` : null;
+  });
+
+  navOverlay.addEventListener('click', () => {
+    navOverlay.classList.remove('nav__overlay--visible');
+    navList.classList.remove('nav__list--visible');
+    navTrigger.classList.remove('nav__trigger--active');
+
+    navTrigger.textContent = navTrigger.getAttribute('data-open');
+    navTrigger.style.transform = null;
+  });
+
   const navLinkClassName = 'nav__link';
   const navLinks = document.querySelectorAll(`.${navLinkClassName}`);
 
