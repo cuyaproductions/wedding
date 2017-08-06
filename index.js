b5315d3e3293c71a18200dae1f1587e777eaf18b
Diego Hernandez <Diego.Hernandez@ibm.com>
2017-08-06 18:18:07 -0500

    Remove any mention of passport or sessions

--- index.js
+++ index.js
@@ -5,21 +5,16 @@ import hbs from 'hbs';
 import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
-import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import session from 'express-session';
-import passport from 'passport';
 
 import findLanguage from './middleware/international';
 import sanitizeBody from './middleware/sanitizeBody';
 import configHbs from './config/hbs';
-import configPassport from './config/passport';
 import router from './routes';
 
 const app = express();
 
 dotenv.config();
-configPassport(passport);
 configHbs(hbs, app);
 
 mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
@@ -35,17 +30,11 @@ app.locals.appName = 'Nic + Diego';
 app.engine('hbs', hbs.__express);
 app.set('view engine', 'hbs');
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+if (process.env.ENV === 'dev') {
+  app.use(morgan('dev'));
+}
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/static', express.static(path.resolve(__dirname, 'dist')));
 app.use(sanitizeBody);
